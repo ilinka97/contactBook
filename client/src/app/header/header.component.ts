ce52d72a20709ae943f91fb5c6a17bed1d6b08d0
Ilinka Poletan <ilinka.p97@gmail.com>
2019-07-01 18:47:00 +0200

    Add Header and set right prefix to root component in index.html

---
+++ client/src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cb-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  userName: String = 'Name';
+
+  onLogout() {
+
+  }
+}
