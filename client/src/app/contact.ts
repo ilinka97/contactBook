51e3f5d1a5d533d27c5caebe438e18861b0756b4
Ilinka Poletan <ilinka.p97@gmail.com>
2019-07-19 16:28:46 +0200

    Add Contact model to frontend

---
+++ client/src/app/contact.ts
@@ -0,0 +1,5 @@
+export class Contact {
+    constructor(public contactName: string, public phoneNumber: string, public email: string,
+        public groupType: string, public address: string, public photoUrl: string
+    ) { }
+}
