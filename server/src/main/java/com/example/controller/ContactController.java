e84d0a4cbd180573ba4a648497e616149d504b1b
Ilinka Poletan <ilinka.p97@gmail.com>
2019-07-29 17:47:25 +0200

    Add controller method for Contact updating

--- server/src/main/java/com/example/controller/ContactController.java
+++ server/src/main/java/com/example/controller/ContactController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -38,5 +39,10 @@ public class ContactController {
 	@GetMapping("/getContact/{id}")
 	public Contact getContactById(@PathVariable Long id) {
 		return contactService.findContactById(id);
+	}
+	
+	@PutMapping("/updateContact")
+	public Contact updateContact(@RequestBody Contact contact) {
+		return contactService.saveContact(contact);
 	}
 }
