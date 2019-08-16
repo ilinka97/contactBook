0853430549ca5eacccdf53ccc45f7f4963722880
Ilinka Poletan <ilinka.p97@gmail.com>
2019-08-16 16:26:05 +0200

    Update RegisterLogin component

--- client/src/app/register-login/register-login.component.ts
+++ client/src/app/register-login/register-login.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'cb-register-login',
-  templateUrl: './register-login.component.html',
-  styleUrls: ['./register-login.component.css']
+  selector: "cb-register-login",
+  templateUrl: "./register-login.component.html",
+  styleUrls: ["./register-login.component.css"]
 })
 export class RegisterLoginComponent implements OnInit {
+  signupForm: FormGroup;
+  loginForm: FormGroup;
 
-  constructor() { }
-
+  constructor(private formBuilder: FormBuilder) {}
   ngOnInit() {
+    this.initSignupForm();
+    this.initLoginForm();
   }
 
+  initSignupForm() {
+    let username: string = "";
+    let email: string = "";
+    let password: string = "";
+
+    this.signupForm = this.formBuilder.group({
+      username: [username, Validators.required],
+      email: [email, Validators.required, Validators.email],
+      password: [password, Validators.required]
+    });
+  }
+  initLoginForm() {
+    let username: string = "";
+    let password: string = "";
+
+    this.loginForm = this.formBuilder.group({
+      username: [username, Validators.required],
+      password: [password, Validators.required]
+    });
+  }
+  onSignup() {}
+  onLogin() {}
 }
