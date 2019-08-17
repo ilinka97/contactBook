085bbda3cb354b140cd105864588c9652ed600c5
Ilinka Poletan <ilinka.p97@gmail.com>
2019-08-17 18:39:31 +0200

    Create User model and add models folder on frontend

---
+++ client/src/app/models/user.ts
@@ -0,0 +1,3 @@
+export class User {
+  constructor(public userId: string, public username: string, public email: string, public password: string) {}
+}
--- client/src/app/services/contact.service.ts
+++ client/src/app/services/contact.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Contact } from 'app/contact';
+import { Contact } from 'app/models/contact';
 
 @Injectable()
 export class ContactService {
