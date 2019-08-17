ae2fc0bb3527a713d215771a8366d4df0f73ad22
Ilinka Poletan <ilinka.p97@gmail.com>
2019-08-17 18:10:56 +0200

    Update Spring Security configuration

--- server/src/main/java/com/example/configuration/SecurityConfiguration.java
+++ server/src/main/java/com/example/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.example.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -28,5 +29,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		auth
 			.userDetailsService(userDetailsService)
 			.passwordEncoder(encoder());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/register", "/login").permitAll()
+				.anyRequest().authenticated();
 	}
 }
