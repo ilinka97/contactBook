27ce2b32586d5b27ae18cf110d00a8d29ad11d60
Ilinka Poletan <ilinka.p97@gmail.com>
2019-09-09 19:42:18 +0200

    Аdd validation for user registration

--- client/src/app/register-login/register-login.component.ts
+++ client/src/app/register-login/register-login.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { UserService } from "app/services/user.service";
 import { UserCredentials } from "app/models/userCredentials";
 import { AuthenticationService } from "app/services/authentication.service";
+import { MustMatch } from "app/services/match.validator";
 
 @Component({
   selector: "cb-register-login",
@@ -14,6 +15,8 @@ export class RegisterLoginComponent implements OnInit {
   loginForm: FormGroup;
   loginFailed: boolean = this.authenticationService.isLoggedIn();
   submitted: boolean;
+  regSubmitted: boolean;
+  isSignedup: boolean;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -28,15 +31,38 @@ export class RegisterLoginComponent implements OnInit {
   initSignupForm() {
     let userCredentials: UserCredentials = new UserCredentials("", "");
     let email: string = "";
+    let confirmPassword = "";
 
     this.signupForm = this.formBuilder.group({
-      userCredentials: this.formBuilder.group({
-        username: [userCredentials.username, Validators.required],
-        password: [userCredentials.password, Validators.required]
-      }),
-      email: [email, [Validators.required, Validators.email]]
+        userCredentials: this.formBuilder.group({
+          username: [userCredentials.username,[Validators.required, Validators.minLength(6)]],
+          password: [userCredentials.password,[Validators.required, Validators.minLength(8)]]
+        }),
+        email: [email, [Validators.required, Validators.email]],
+        confirmPassword: [confirmPassword, Validators.required]
+      },{
+        validator: MustMatch("userCredentials.password", "confirmPassword")
+      }
+    );
+  }
+  get regUsername() { return this.signupForm.get("userCredentials.username"); }
+  get regPassword() { return this.signupForm.get("userCredentials.password"); }
+  get regConfirmPass() { return this.signupForm.get("confirmPassword"); }
+  get regEmail() { return this.signupForm.get("email"); }
+  onSignup() {
+    this.regSubmitted = true;
+    if (this.signupForm.invalid) {
+      return;
+    }
+    let formValue = this.signupForm.value;
+    delete formValue["confirmPassword"];
+    this.userService.signup(formValue).subscribe(() => {
+      this.signupForm.reset();
+      this.regSubmitted = false;
+      this.isSignedup = true;
     });
   }
+
   initLoginForm() {
     let credentials: UserCredentials = new UserCredentials("", "");
 
@@ -47,13 +73,6 @@ export class RegisterLoginComponent implements OnInit {
   }
   get username() { return this.loginForm.get("username"); }
   get password() { return this.loginForm.get("password"); }
-  onSignup() {
-    let formValue = this.signupForm.value;
-    this.userService.signup(formValue).subscribe(
-      () => {
-        this.signupForm.reset();
-    });
-  }
   onLogin() {
     this.submitted = true;
     if (this.loginForm.invalid) {
